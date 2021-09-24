fa1f905fc5c9f7b95211efcb6d6efc75acb215f4
jorgen-fidjeland <90451490+jorgen-fidjeland@users.noreply.github.com>
2021-09-24 16:09:03 +0200

    Update Autoflens_script.py

--- AVVA.tab/Autoflens.panel/Autoflens.pushbutton/Autoflens_script.py
+++ AVVA.tab/Autoflens.panel/Autoflens.pushbutton/Autoflens_script.py
@@ -500,7 +500,7 @@ for i in EQ:
                             new_flange.Location.Move(1232131)
                         except:
                             status = status + ' Feil ved flytting.'
-                            DeleteEntity(new_flange)
+                            doc.Delete(new_flange.Id)
                             continue
 
                         ########################
