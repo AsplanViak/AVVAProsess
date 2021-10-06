70b0f3e06f6cd3c8ef2d601f2167ddc60ef5f9cc
jorgen-fidjeland <90451490+jorgen-fidjeland@users.noreply.github.com>
2021-10-06 14:00:39 +0200

    Update VelgRorKrageLosflens_script.py

--- AVRIMP.tab/Autoflens.panel/KrageLøsflens.pulldown/VelgRørKrageLøsflens.pushbutton/VelgRorKrageLosflens_script.py
+++ AVRIMP.tab/Autoflens.panel/KrageLøsflens.pulldown/VelgRørKrageLøsflens.pushbutton/VelgRorKrageLosflens_script.py
@@ -242,7 +242,7 @@ def CheckValveConnectors(valve_family):
     #famdoc.LoadFamilyDocOpt(Document = doc, IFamilyLoadOptions = FamOpt1())
     print('changed :' + str(changed))
     if changed:
-        famdoc.LoadFamily.Overloads.Functions[3](Document=doc, IFamilyLoadOptions=FamOpt1())
+        famdoc.LoadFamily.Overloads.Functions[6](Document=doc, IFamilyLoadOptions=FamOpt1())
 
     famdoc.Close(False)
 
