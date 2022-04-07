fd22d965db9fdfc174728ec94a8fa900f7980e30
jorgen-fidjeland <90451490+jorgen-fidjeland@users.noreply.github.com>
2022-04-07 11:03:50 +0200

    Update SyncEldata_script.py

--- AVRIMP.tab/UnderUtvikling.panel/IkkeBruk.pulldown/SyncEldata.pushbutton/SyncEldata_script.py
+++ AVRIMP.tab/UnderUtvikling.panel/IkkeBruk.pulldown/SyncEldata.pushbutton/SyncEldata_script.py
@@ -156,7 +156,7 @@ def DecodeIfString(cellValue):
     if isinstance(cellValue, str):
         print('decoding ' + cellValue)
         #return cellValue.decode(encoding = 'utf-8', errors = 'replace')  #ignore
-        print cellCalue.type()
+        print cellValue.type()
         #print('decoded: ' + cellValue.decode(encoding='utf-8'))
         #print('encoded: ' + cellValue.encode(encoding='utf-8'))
         #return cellValue.decode(encoding='utf-8')
