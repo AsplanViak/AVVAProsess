443cd73de4ab7360369dcfc13ac1a102c392abd6
jorgen-fidjeland <90451490+jorgen-fidjeland@users.noreply.github.com>
2022-05-24 11:50:12 +0200

    Update SyncEldata_script.py

--- AVRIMP.tab/SynkroniserEldata.panel/SyncEldata.pushbutton/SyncEldata_script.py
+++ AVRIMP.tab/SynkroniserEldata.panel/SyncEldata.pushbutton/SyncEldata_script.py
@@ -291,6 +291,7 @@ def MainFunction():
     if not sentralfil:
         prosjektnavn = 'testprosjekt'
     else:
+        sentralfil = sentralfil.replace('_Sentralfil').replace('')
         sentralfil = sentralfil.replace('\\', '/').replace('.', '/')
         sentralfil = sentralfil.split('/')
         prosjektnavn = sentralfil[-2]
