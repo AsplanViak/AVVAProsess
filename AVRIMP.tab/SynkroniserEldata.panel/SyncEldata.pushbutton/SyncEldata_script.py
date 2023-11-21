ec8bb7548479636d97f3d636e80bcc24ce1dd186
jorgenfidjeland <jorgen.fidjeland@asplanviak.no>
2023-11-21 15:46:34 +0100

    Update SyncEldata_script.py

--- AVRIMP.tab/SynkroniserEldata.panel/SyncEldata.pushbutton/SyncEldata_script.py
+++ AVRIMP.tab/SynkroniserEldata.panel/SyncEldata.pushbutton/SyncEldata_script.py
@@ -724,28 +724,22 @@ def MainFunction():
             elif tag_cat_status == 2:
                 DebugPrint('tag_cat_status 2 start ')
                 # skjema TFM
-                #if (1):
-                try:
-                    sysnr = i.LookupParameter('Systemnummer').AsString()
-                    DebugPrint(sysnr)
-                except:
-                    DebugPrint('gikk i dass')
                 try:
                     #SystemVar = i.LookupParameter('SystemVar').AsString()
                     #TFM11FkKompLNR = i.LookupParameter('TFM11FkKompLNR').AsString()
                     #TFM11FkKompGruppe = i.Symbol.LookupParameter('TFM11FkKompGruppe').AsString()
                     #tag = r"'" + '=' + SystemVar + '-' + TFM11FkKompGruppe + TFM11FkKompLNR
-                    DebugPrint('tag_cat_status 2 try start')
-                    TFMkode = i.LookupParameter('TFM-kode').AsString()
-                    DebugPrint('tag_cat_status 2 try 2')
-                    Systemnummer = i.LookupParameter('Systemnummer').AsString()
+                    #DebugPrint('tag_cat_status 2 try start')
+                    TFMkode = k.LookupParameter('TFM-kode').AsString()
+                    #DebugPrint('tag_cat_status 2 try 2')
+                    Systemnummer = k.LookupParameter('Systemnummer').AsString()
                     #Bør trolig legge inn sjekk her mot tomme verdier
-                    DebugPrint('tag_cat_status 2 ')
+                    #DebugPrint('tag_cat_status 2 ')
                     if TFMkode !='' and Systemnummer != '' :
                         tag = Systemnummer + '-' + TFM11kode
-                        DebugPrint('Sammenslått tag: ' + tag)
+                        #DebugPrint('Sammenslått tag: ' + tag)
                     else:
-                        DebugPrint('Klarte ikke lage sammenslått tag')
+                        #DebugPrint('Klarte ikke lage sammenslått tag')
                         continue
                 except:
                     SummaryPrint('feil ved sammenslåing/avlesing av parametre som inngår i TFM for skjema')
